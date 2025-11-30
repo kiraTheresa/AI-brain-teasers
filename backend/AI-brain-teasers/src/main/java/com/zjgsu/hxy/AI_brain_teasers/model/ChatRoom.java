50f782d5bf946a4259b9f1f542ac64167b676da5
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2025-11-30 18:52:13 +0800

    向阳路

--- backend/AI-brain-teasers/src/main/java/com/zjgsu/hxy/AI_brain_teasers/model/ChatRoom.java
+++ backend/AI-brain-teasers/src/main/java/com/zjgsu/hxy/AI_brain_teasers/model/ChatRoom.java
@@ -1,12 +1,25 @@
 package com.zjgsu.hxy.AI_brain_teasers.model;
 
 import com.volcengine.ark.runtime.model.completion.chat.ChatMessage;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class ChatRoom {
     private Long roomId;
     private List<ChatMessage> chatMessageList;
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public List<ChatMessage> getChatMessageList() {
+        return chatMessageList;
+    }
+
+    public void setChatMessageList(List<ChatMessage> chatMessageList) {
+        this.chatMessageList = chatMessageList;
+    }
 }
